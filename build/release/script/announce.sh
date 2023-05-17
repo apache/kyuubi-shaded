fc69f8862b981188f176201a534abd750f292cc2
Cheng Pan <chengpan@apache.org>
2023-05-17 22:33:45 +0800

    [KYUUBI-SHADED #9] Add scripts for Apache release
    ### _Why are the changes needed?_
    
    It's required for Apache Release. Notes, the source artifacts are enforced by Apache policy, and we don't need to publish binary artifacts, in addition, we should deploy the jar to the maven central.
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.readthedocs.io/en/master/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #9 from pan3793/apache-release.
    
    f9ed1fb [Cheng Pan] nit
    d6b4534 [Cheng Pan] Add scripts for Apache release
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

---
+++ build/release/script/announce.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -o pipefail
+set -e
+
+RELEASE_DIR="$(cd "$(dirname "$0")"/..; pwd)"
+
+######### Please modify the variables ##########
+# release version, e.g. 0.1.0
+release_version=${release_version:-""}
+################################################
+
+if [[ -z $release_version ]]; then
+  echo "Please input release version"
+  exit 1
+fi
+
+echo "Release version: ${release_version}"
+
+RELEASE_TEMP_DIR=${RELEASE_DIR}/tmp
+mkdir -p ${RELEASE_TEMP_DIR}
+ANNOUNCE=${RELEASE_TEMP_DIR}/${release_version}_announce.temp
+
+cat >$ANNOUNCE<<EOF
+Title: [ANNOUNCE] Apache Kyuubi Shaded released ${release_version}
+
+Content:
+Hi all,
+
+The Apache Kyuubi community is pleased to announce that
+Apache Kyuubi Shaded ${release_version} has been released!
+
+The full release notes are available at:
+Release Notes: https://kyuubi.apache.org/release/kyuubi-shaded-${release_version}.html
+
+To learn more about Apache Kyuubi, please see
+https://kyuubi.apache.org/
+
+Kyuubi Resources:
+- Issue: https://github.com/apache/kyuubi/issues
+- Mailing list: dev@kyuubi.apache.org
+
+We would like to thank all contributors of the Kyuubi community
+who made this release possible!
+
+Thanks,
+On behalf of Apache Kyuubi community
+EOF
+
+echo "please Use announce@apache.org, dev@kyuubi.apache.org
+see announce content in $ANNOUNCE"
